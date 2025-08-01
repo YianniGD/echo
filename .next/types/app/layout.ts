c02e1a63d35e1d017adb17b2181389216cb52311
Platinumtyrant <ygaliatsatos@gmail.com>
2025-08-01 12:12:23 -0400

    edit

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /Users/yiannigaliatsatos/Documents/GitHub/echo/echo/src/app/layout.tsx
+// File: /Users/yiannigaliatsatos/Documents/GitHub/echo/src/app/layout.tsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
