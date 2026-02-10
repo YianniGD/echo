0a337fe140ad4814d898537cdc8c87ce905deb5f
Yianni Galiatsatos <70313200+YianniGD@users.noreply.github.com>
2026-02-09 19:48:39 -0500

    security update

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/yiannigaliatsatos/Documents/GitHub/echo/src/app/page.tsx
+// File: /Users/yiannigaliatsatos/Documents/Antigravity/echo/src/app/page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
